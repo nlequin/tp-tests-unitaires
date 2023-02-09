883cd07633cde3be9a9fb0d26f72c727b35d10bc
Nicolas <nicolas.lequin@celsius-online.com>
2023-02-09 14:18:06 +0100

    Uncomment

--- app/Calculator.py
+++ app/Calculator.py
@@ -48,7 +48,7 @@ def calculate(operation, x, y):
         result = Calculator.square_root(x)
     return result
 
-# operation = input("Enter the operation you would like to perform (add, subtract, multiply, divide, square_root, power): ")
-# num1 = int(input("Enter the first number : "))
-# num2 = int(input("Enter the second number : "))
-# print(calculate(operation, num1, num2))
+operation = input("Enter the operation you would like to perform (add, subtract, multiply, divide, square_root, power): ")
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+print(calculate(operation, num1, num2))
